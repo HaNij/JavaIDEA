7be590e92efa0daf95f2bdc41a0ec44879a2553b
Kirill <kirhany102@gmail.com>
2017-05-04 18:12:59 +0500

    Adding core system with numbers

--- src/com/huni/javafx/GEN_PASSWORD/Main.java
+++ src/com/huni/javafx/GEN_PASSWORD/Main.java
@@ -1,6 +1,21 @@
 package com.huni.javafx.GEN_PASSWORD;
-
+import java.util.Random;
 
 public class Main {
+    private static final String numbers = "0123456789";
 
+    private static String generatePassword(int count) {
+        String out = "";
+        Random rand = new Random();
+
+        for (int i = 0; i < count; i++) {
+            out += numbers.charAt(rand.nextInt(numbers.length()));
+        }
+
+        return out;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generatePassword(12));
+    }
 }
